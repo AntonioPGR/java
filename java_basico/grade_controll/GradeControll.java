3111a312d9424591b3402a2d54bb68ae78883a7d
Antonio Pacheco Gerim Ribeiro <antoninhopgr@gmail.com>
2023-09-13 22:34:41 -0300

    GradeControll

---
+++ java_basico/grade_controll/GradeControll.java
@@ -0,0 +1,25 @@
+package grade_controll;
+
+import java.util.Scanner;
+
+public class GradeControll {
+
+  public static void main(String[] args) {
+    System.out.println("Enter the user grade:");
+    int grade = getGrade();
+
+    if (grade < 7) {
+      System.out.println("This student has flunked");
+    } else {
+      System.out.println("This student passed");
+    }
+  }
+
+  static int getGrade() {
+    Scanner teclado = new Scanner(System.in);
+    String input = teclado.nextLine();
+    teclado.close();
+    return Integer.parseInt(input);
+  }
+
+}
